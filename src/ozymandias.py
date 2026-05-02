a268b22f0f8b02fb751be616e48e9f2631bcc61e
Chrys Bishop <phoebesbishop@gmail.com>
2026-05-02 17:26:44 -0500

    added Button class

--- src/ozymandias.py
+++ src/ozymandias.py
@@ -1,10 +1,35 @@
 import pygame
 
-    ## Week 1: 
-    # figure out resolution (600 x 600) some screens may be larger than the resolution, in which we can pan around
-    # create elements (images)
-       # 
-    # mouse work
+
+class Animation():
+
+    def __init__(self, life):
+        self.life = life
+
+
+class Button():
+
+    def __init__(self, pos=(0,0), size=25):
+        self.pos = pos
+        self.size = size
+        self.color = pygame.Color(0, 255, 255)
+        self.alpha = 255
+        self.dead = False
+        self.surface = self.update_surface()
+        self.area = (self.pos, (self.pos[0] + self.size, 
+                                self.pos[1] + self.size)) # min x, min y, max x, max y
+
+    def update_surface(self):
+        surf = pygame.Surface((self.size*0.8, self.size*0.8))
+        surf.fill(self.color)
+        return surf
+
+    def draw(self, surface):
+        if self.dead:
+            return
+        self.surface.set_alpha(self.alpha)        
+        surface.blit(self.surface, self.pos)
+
 
 def main():
 
@@ -13,11 +38,15 @@ def main():
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
     running = True
-
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        button = Button()
+        button.draw(screen)
+        pygame.display.update()
 
 
 
